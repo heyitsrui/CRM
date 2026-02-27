c4bcdfac7f01fd8e1c34fde3b8b1b3db60b37f71
Ritz <187958702+Ritzqtt@users.noreply.github.com>
2026-02-27 18:54:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1158,5 +1158,3 @@ app.put("/api/timetree/chat/:chatId", async (req, res) => {
   // ================= SERVER =================
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
